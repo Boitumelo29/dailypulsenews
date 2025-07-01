c4cdbe80c7662ed4072de8682ed7e6d1ee42428c
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-07-01 17:54:32 +0200

    WIP: adding setupDependencies to bootstrap.dart

--- lib/core/bootstrap/bootstrap.dart
+++ lib/core/bootstrap/bootstrap.dart
@@ -1,12 +1,13 @@
 import 'package:dailypulsenews/core/app/app.dart';
 import 'package:dailypulsenews/core/bloc_observer/app_bloc_observer.dart';
+import 'package:dailypulsenews/core/dependency_injection/di.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await AppInitializer.initialize();
-  // setupDependencies();
+  setupDependencies();
   Bloc.observer = AppBlocObserver();
   runApp(App());
 }
