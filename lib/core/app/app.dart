95c49fbd419fc70ab304699309fe1d4de030ec2d
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-07-01 14:51:33 +0200

    WIP: creating app and setting up theme as well as adding screenUtil and adding router

---
+++ lib/core/app/app.dart
@@ -0,0 +1,36 @@
+import 'package:dailypulsenews/core/routers/router.dart';
+import 'package:dailypulsenews/l10n/app_localizations.dart';
+import 'package:dailypulsenews/core/utils/theme/colour_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class App extends StatelessWidget {
+  App({super.key});
+
+  final _appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child) => MaterialApp.router(
+        themeMode: ThemeMode.system,
+        theme: ThemeData(
+            colorScheme: ColourScheme.lightScheme, useMaterial3: true),
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en'),
+        ],
+        debugShowCheckedModeBanner: false,
+        routerDelegate: _appRouter.delegate(),
+        routeInformationParser: _appRouter.defaultRouteParser(),
+      ),
+    );
+  }
+}
