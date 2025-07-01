8d6285363997c79c33231b109a9bb674d5639a90
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-07-01 21:28:39 +0200

    WIP:adding showCountryListBottomSheet to headlines_view.dart and adding rout to headlines_view.dart to

--- lib/feature/headlines/presentation/pages/headlines_view.dart
+++ lib/feature/headlines/presentation/pages/headlines_view.dart
@@ -1,34 +1,60 @@
-import 'package:dailypulsenews/core/extenstions/localization_extensions.dart';
+import 'package:auto_route/annotations.dart';
+import 'package:dailypulsenews/core/core.dart';
 import 'package:dailypulsenews/feature/headlines/application/bloc/news_bloc.dart';
 import 'package:dailypulsenews/feature/headlines/presentation/widgets/headline_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeView extends StatelessWidget {
+@RoutePage()
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final List<Map<String, String>> countries = const [
+    {'name': 'United States', 'code': 'US'},
+    {'name': 'Canada', 'code': 'CA'},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(context.loc.dailyPulseNews),
-        ),
-        body: BlocConsumer<NewsBloc, NewsState>(
-          listener: _listener,
-          builder: (context, state) {
-            if (state.loading) {
-              return const Center(child: CircularProgressIndicator());
-            }
+      appBar: AppBar(
+        title: Text(context.loc.dailyPulseNews),
+      ),
+      body: BlocConsumer<NewsBloc, NewsState>(
+        listener: _listener,
+        builder: (context, state) {
+          if (state.loading) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-            return ListView.builder(
-              itemCount: state.articles.length,
-              itemBuilder: (context, index) {
-                final article = state.articles[index];
-                return HeadlineCard(article: article);
-              },
-            );
-          },
-        ));
+          if (state.articles.isEmpty) {
+            return Center(child: Text(context.loc.noNewNews));
+          }
+
+          return ListView.builder(
+            itemCount: state.articles.length,
+            itemBuilder: (context, index) {
+              final article = state.articles[index];
+              return HeadlineCard(article: article);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showCountryListBottomSheet(context);
+        },
+        child: Icon(
+          Icons.change_circle,
+          color: context.colorScheme.onSurface,
+        ),
+      ),
+    );
   }
 
   void _listener(BuildContext context, NewsState state) {
@@ -42,6 +68,80 @@ class HomeView extends StatelessWidget {
         },
         (_) {},
       ),
+    );
+  }
+
+  void _showCountryListBottomSheet(BuildContext context) {
+    final selectedCountry = context.read<NewsBloc>().state.selectedCountry;
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (BuildContext context) {
+        return SizedBox(
+          height: 250,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                     Text(
+                      context.loc.selectCountry,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: countries.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final country = countries[index];
+                    final isSelected = selectedCountry.toUpperCase() ==
+                        country['code']!.toUpperCase();
+
+                    return ListTile(
+                      title: Text(
+                        country['name']!,
+                        style: TextStyle(
+                          fontWeight:
+                              isSelected ? FontWeight.bold : FontWeight.normal,
+                          color: isSelected
+                              ? Theme.of(context).colorScheme.primary
+                              : null,
+                        ),
+                      ),
+                      trailing: isSelected
+                          ? Icon(Icons.check,
+                              color: Theme.of(context).colorScheme.primary)
+                          : null,
+                      onTap: () {
+                        final selected = country['code']!;
+                        context.read<NewsBloc>().add(
+                              NewsEvent.setPreferredCountry(country: selected),
+                            );
+                        Navigator.pop(context);
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
