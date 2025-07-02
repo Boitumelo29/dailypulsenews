186d7a01f95905c353502090fbb33e53a546b759
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-07-02 16:42:05 +0200

    WIP: adding app_initializer.dart to bootstrap.dart

--- lib/core/bootstrap/bootstrap.dart
+++ lib/core/bootstrap/bootstrap.dart
@@ -1,12 +1,12 @@
 import 'package:dailypulsenews/core/app/app.dart';
 import 'package:dailypulsenews/core/bloc_observer/app_bloc_observer.dart';
 import 'package:dailypulsenews/core/dependency_injection/di.dart';
+import 'package:dailypulsenews/core/init/app_initializer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> bootstrap() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // await AppInitializer.initialize();
+   await AppInitializer.initialize();
   setupDependencies();
   Bloc.observer = AppBlocObserver();
   runApp(App());
