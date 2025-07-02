233c5ffd821b44d76b3029c32f49a07a7d5060c5
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-07-02 20:25:06 +0200

    WIP: creating app_provider.dart

---
+++ lib/core/provider/app_provider.dart
@@ -0,0 +1,20 @@
+import 'package:dailypulsenews/core/core.dart';
+import 'package:dailypulsenews/feature/auth/application/bloc/auth_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppProvider extends StatelessWidget {
+  final Widget child;
+
+  const AppProvider({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [
+      BlocProvider(
+          create: (context) => getIt<AuthBloc>()
+            ..add(CheckAuthStatus())
+            ..add(ListenToAuthChanges()))
+    ], child: child);
+  }
+}
