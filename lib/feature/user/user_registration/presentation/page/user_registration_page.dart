18e8bdbd011d698a4a7458b4cf7e2b986099ebd4
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-07-02 20:31:44 +0200

    WIP: adding router to user_registration_page.dart

--- lib/feature/user/user_registration/presentation/page/user_registration_page.dart
+++ lib/feature/user/user_registration/presentation/page/user_registration_page.dart
@@ -1,18 +1,19 @@
 import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:dailypulsenews/core/dependency_injection/di.dart';
 import 'package:dailypulsenews/feature/user/user_registration/application/bloc/user_registration_bloc.dart';
-import 'package:dailypulsenews/feature/user/user_registration/presentation/page/user_registration_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
-class UserRegistrationPage extends StatelessWidget {
-  const UserRegistrationPage({super.key});
+class UserRegistrationWrapperPage extends StatelessWidget {
+  const UserRegistrationWrapperPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => UserRegistrationBloc(),
-      child: const UserRegistrationView(),
+      create: (context) => getIt<UserRegistrationBloc>(),
+      child: AutoRouter(),
     );
   }
 }
