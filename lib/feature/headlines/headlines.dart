0a84527f0ed300c03fedc059a5ebb357c17ee3aa
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-07-03 06:32:49 +0200

    Chore: moving exports to headlines.dart

---
+++ lib/feature/headlines/headlines.dart
@@ -0,0 +1,7 @@
+export 'application/bloc/news_bloc.dart';
+export 'data/datasources/news_api_service.dart';
+export 'data/models/article_model.dart';
+export 'domain/repositories/news_repository.dart';
+export 'domain/repositories/news_repository_impl.dart';
+export 'presentation/widgets/drawer.dart';
+export 'presentation/widgets/headline_card.dart';
