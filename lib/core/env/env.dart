729b0998169f6d4c019f782153d0874c9e5d695c
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-07-03 14:25:51 +0200

    Fix: fixing env error

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -5,12 +5,6 @@ part 'env.g.dart';
 @Envied(path: '.env')
 @envied
 abstract class Env {
-  @EnviedField(varName: 'API_URL', obfuscate: true)
-  static  String apiURL = _Env.apiURL;
-
-  @EnviedField(varName: 'API_KEY', obfuscate: true)
-  static  String apiKey = _Env.apiKey;
-
   @EnviedField(varName: 'NEWS_API_KEY', obfuscate: true)
   static  String newsApiKey = _Env.newsApiKey;
 
